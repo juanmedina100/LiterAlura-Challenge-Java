074b7b8965b1d226ca702f65e7e6bba88f8da0d7
juanmedina100 <92758289+juanmedina100@users.noreply.github.com>
2024-05-20 16:18:42 -0600

    commit inicial

---
+++ src/test/java/com/jimd/LiterAluraChallengeJava/LiterAluraChallengeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimd.LiterAluraChallengeJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
